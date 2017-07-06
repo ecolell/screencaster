0cecf574107cf90d38e35970f8ffae853ab190dc
eloy <eloy.colell@gmail.com>
2017-07-06 13:30:59 -0300

    Introduce the rails_helper.rb as required by Rspec 3.x.

--- spec/features/episodes_spec.rb
+++ spec/features/episodes_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe "Episodes" do
   describe "index action" do
--- spec/models/episode_spec.rb
+++ spec/models/episode_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe Episode do
   it "validates presence of name" do
